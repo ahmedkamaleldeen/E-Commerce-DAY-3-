14bb1566209d4e92b3e324ecf2d4d3d6f4a7ebd7
ahmedkamaleldeen <ahmedkamaleldeen96@gmail.com>
2022-02-20 22:00:04 +0200

    E-Commerce(DAY#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductcardComponent } from './productcard/productcard.component';
+import { CoinPipe } from './coin.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    ProductcardComponent,
+    CoinPipe
   ],
   imports: [
     BrowserModule,
